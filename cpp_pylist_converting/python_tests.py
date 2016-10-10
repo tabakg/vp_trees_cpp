0610fde7ca22558141340998fafe9c75d6738d23
Gil Tabak <tabak.gil@gmail.com>
2016-10-10 14:51:30 -0700

    moved files

--- cpp_pylist_converting/python_tests.py
+++
@@ -1,5 +0,0 @@
-import cpp_conversions
-
-mat = [[3,4,5,6],[4,5,6,6]];
-print cpp_conversions.list_matrix_list_test([[3,4,5,6],[4,5,6,6]]);
-print cpp_conversions.list_double_vec_list_test([[3,4,5,6],[4,5,6,6]]);
